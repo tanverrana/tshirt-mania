fe1397babbbf7775641cbb98f280ce5b1bc071b1
tanverrana <tanversobur@gmail.com>
2022-04-03 04:28:45 +0600

    create routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import OrderReview from './components/OrderReview/OrderReview';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/orderreview" element={<OrderReview></OrderReview>}></Route>
+      </Routes>
     </div>
   );
 }
