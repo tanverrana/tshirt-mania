ccdc34a06579387b8578af0ad850fff6b7cf3b7a
tanverrana <tanversobur@gmail.com>
2022-04-03 11:14:50 +0600

    conditional rendering

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import "./Cart.css";
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
     //Conditional Rendering option
     //1.Element Variable
+    //2. Ternary operator condition ? true :false
+    //3. && Operator
+    //4.|| operator
     let command;
     if (cart.length === 0) {
         command = <div>
@@ -20,12 +24,19 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
         <div>
             <h2>This is cart.</h2>
             <h3>Item Selected: {cart.length}</h3>
-            {command}
+
             {
                 cart.map(tshirt => <p>{tshirt.name}
                     <button onClick={() => handleRemoveFromCart(tshirt)}>Remove</button>
                 </p>)
             }
+            {cart.length === 0 || <p className="orange">Yay! You are Buying</p>}
+            {cart.length === 2 && <div className="orange">
+                <h3>Try Go now!</h3>
+            </div>}
+            {command}
+            {cart.length !== 4 ? <p>Keep Adding</p> : <button>Clear all</button>}
+            {cart.length === 4 && <button className="orange">Review Order</button>}
         </div>
     );
 };
